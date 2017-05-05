d38a704f17d84c9e275cddabb367f40bb8731637
ledicjp <ledicjp@gmail.com>
2017-05-05 23:59:02 +0200

    Added Angular-CLI project under org.eclipse.winery.topologymodeler.ui

--- org.eclipse.winery.topologymodeler.ui/e2e/app.e2e-spec.ts
+++ org.eclipse.winery.topologymodeler.ui/e2e/app.e2e-spec.ts
@@ -1,15 +1,14 @@
-import { browser, element, by } from 'protractor';
+import { Org.Eclipse.Winery.Topologymodeler.UiPage } from './app.po';
 
-describe('QuickStart E2E Tests', function () {
+describe('org.eclipse.winery.topologymodeler.ui App', () => {
+  let page: Org.Eclipse.Winery.Topologymodeler.UiPage;
 
-  let expectedMsg = 'Hello Angular';
-
-  beforeEach(function () {
-    browser.get('');
+  beforeEach(() => {
+    page = new Org.Eclipse.Winery.Topologymodeler.UiPage();
   });
 
-  it('should display: ' + expectedMsg, function () {
-    expect(element(by.css('h1')).getText()).toEqual(expectedMsg);
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
   });
-
 });
