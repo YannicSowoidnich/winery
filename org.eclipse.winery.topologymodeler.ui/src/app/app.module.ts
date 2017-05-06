55da6909ed3db4355b0344d888d7d0f8cb89d23f
ledicjp <ledicjp@gmail.com>
2017-05-06 03:02:21 +0200

    Added Angular-CLI project. Developed a navbar styled with ngx-bootstrap4 Enabled bootstrap4 support. Edited angular-cli.json.

--- org.eclipse.winery.topologymodeler.ui/src/app/app.module.ts
+++ org.eclipse.winery.topologymodeler.ui/src/app/app.module.ts
@@ -2,17 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {BsDropdownModule} from 'ngx-bootstrap/dropdown';
 
 import { AppComponent } from './app.component';
+import {NavbarComponent} from './navbar/navbar.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    BsDropdownModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
