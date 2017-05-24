fdb10d460c63b680f847f27ef7bef5dc56eb84e6
yannic <yannic.sowoidnich@gmx.de>
2017-05-24 12:40:27 +0200

    Smoothed the styling of palette and added some animations

--- org.eclipse.winery.topologymodeler.ui/src/app/palette/palette.component.ts
+++ org.eclipse.winery.topologymodeler.ui/src/app/palette/palette.component.ts
@@ -1,13 +1,61 @@
 import { Component, OnInit } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { PaletteService } from '../palette.service';
+
 @Component({
   selector: 'app-palette-component',
   templateUrl: './palette.component.html',
   styleUrls: ['./palette.component.css'],
   providers: [PaletteService],
+  animations: [
+    trigger('paletteRootState', [
+      state('shrunk', style({
+        height: '100px',
+        width: '40px',
+      })),
+      state('extended', style({
+        height: '40px',
+        width: '100%',
+      })),
+      transition('shrunk => extended', animate('1s ease-out')),
+      transition('extended => shrunk', animate('1s ease-out'))
+    ]),
+
+    trigger('paletteRootTextState', [
+      state('shrunk', style({
+        transform: 'rotate(270deg)',
+        textAlign: 'center',
+        marginTop: '40px',
+      })),
+      state('extended', style({
+        textAlign: 'center',
+        marginTop: '0px',
+        transform: 'rotate(360deg)',
+      })),
+      transition('shrunk => extended', animate('1s ease-out')),
+      transition('extended => shrunk', animate('1s ease-out'))
+    ]),
+
+    trigger('paletteItemState', [
+      state('shrunk', style({
+        display: 'none',
+        opacity: '0',
+        width: '40px',
+      })),
+      state('extended', style({
+        display: 'block',
+        opacity: '1',
+        width: '100%',
+      })),
+      transition('shrunk => extended', animate('1s ease-out')),
+      transition('extended => shrunk', animate('1s ease-out'))
+    ])
+  ]
 })
 export class PaletteComponent implements OnInit {
-  isHidden = true;
+  detailsAreHidden = true;
+  paletteIsShrunk = true;
+  paletteRootState = 'shrunk';
   paletteItems = [];
 
   constructor(private paletteService: PaletteService) {
@@ -18,10 +66,18 @@ export class PaletteComponent implements OnInit {
   }
 
   public openPalette(): void {
-    this.isHidden = false;
+    this.detailsAreHidden = false;
+    this.toggleRootState();
 
   }
 
+  public toggleRootState(): void {
+    if (this.paletteRootState === 'shrunk') {
+      this.paletteRootState = 'extended';
+    } else {
+      this.paletteRootState = 'shrunk';
+    }
+  }
 }
 
 
