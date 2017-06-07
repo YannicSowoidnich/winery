e677959cff8cbdf7dffcde159cbf8c57ba4fe1d8
yannic <yannic.sowoidnich@gmx.de>
2017-06-07 15:07:31 +0200

    worked on the layout directive

--- org.eclipse.winery.topologymodeler.ui/src/app/layout.directive.ts
+++ org.eclipse.winery.topologymodeler.ui/src/app/layout.directive.ts
@@ -1,12 +1,13 @@
-import { Directive, ElementRef, Input, HostListener, ViewChild} from '@angular/core';
+import { AfterViewInit, Directive, ElementRef, HostListener, Input, ViewChild } from '@angular/core';
+import { CanvasComponent } from './canvas/canvas.component';
 
 @Directive({
-  selector: '[appLayout]'
+  selector: '[appLayout]',
 })
-export class LayoutDirective {
+export class LayoutDirective implements AfterViewInit {
   @Input() color: any;
   @Input() appLayout: any;
-  @ViewChild ('canvas') canvas;
+  @ViewChild('test') canvas;
 
   constructor(el: ElementRef) {
     console.log(el);
@@ -15,7 +16,12 @@ export class LayoutDirective {
   @HostListener('click') onClick() {
     this.layout(['1', '2', '3']);
   }
+
   public layout(elements: string[]): void {
+    console.log(this.canvas);
+  }
+
+  ngAfterViewInit() {
     console.log(this.canvas.nativeElement.children);
   }
 }
