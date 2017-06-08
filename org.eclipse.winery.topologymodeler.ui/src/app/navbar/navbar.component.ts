e47a54979cb494622febf65c80d23ec21948b51b
Thommy Zelenik <t.zelenik@live.de>
2017-06-08 22:39:58 +0200

    Changed the component structure

--- org.eclipse.winery.topologymodeler.ui/src/app/navbar/navbar.component.ts
+++ org.eclipse.winery.topologymodeler.ui/src/app/navbar/navbar.component.ts
@@ -9,7 +9,7 @@
  * Contributors:
  *     Josip Ledic - initial API and implementation
  */
-import {Component, EventEmitter, Input, OnInit, Output, AfterViewInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {WineryAlertService} from '../winery-alert/winery-alert.service';
 import {LayoutDirective} from '../layout.directive';
 import {SharedNodeNavbarService} from '../shared-node-navbar.service';
@@ -20,11 +20,7 @@ import {SharedNodeNavbarService} from '../shared-node-navbar.service';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-  isActive = false;
-  titleOfClickedItem = [];
-  numberOfApacheInstances = 1;
-  numberOfBabelInstances = 1;
-  numberOfDashboardInstances = 1;
+
   buttonPressed = false;
 
   constructor(private alert: WineryAlertService, private _sharedNodeNavbarService: SharedNodeNavbarService) {
@@ -54,49 +50,5 @@ export class NavbarComponent implements OnInit {
   }
 
   ngOnInit() {
-  }
-
-  generateIDOfNode($event: any): void {
-    if ($event === 'Apache-2.4') {
-      if (this.numberOfApacheInstances === 1) {
-        const title = $event;
-        this.titleOfClickedItem.push(title);
-      } else {
-        const title = $event;
-        const instanceNumber = this.numberOfApacheInstances.toString();
-        const fullID = title.concat('_' + instanceNumber);
-        this.titleOfClickedItem.push(fullID);
-      }
-      this.numberOfApacheInstances++;
-    } else if ($event === 'Babel') {
-      if (this.numberOfBabelInstances === 1) {
-        const title = $event;
-        this.titleOfClickedItem.push(title);
-      } else {
-        const title = $event;
-        const instanceNumber = this.numberOfBabelInstances.toString();
-        const fullID = title.concat('_' + instanceNumber);
-        this.titleOfClickedItem.push(fullID);
-      }
-      this.numberOfBabelInstances++;
-    } else if ($event === 'Dashboard') {
-      if (this.numberOfDashboardInstances === 1) {
-        const title = $event;
-        this.titleOfClickedItem.push(title);
-      } else {
-        const title = $event;
-        const instanceNumber = this.numberOfDashboardInstances.toString();
-        const fullID = title.concat('_' + instanceNumber);
-        this.titleOfClickedItem.push(fullID);
-      }
-      this.numberOfDashboardInstances++;
-    }
-  }
-
-  appendTitleToArray($event) {
-    this.generateIDOfNode($event);
-    this.isActive = true;
-    console.log('fuck');
-    console.log(this.titleOfClickedItem);
   }
 }
